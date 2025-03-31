1cd3d8e8415b7546030a9e54bf487e7cc7c57047
roflan635 <nikolaev.nikita2281337@gmail.com>
2025-03-31 17:56:32 +0400

    много разных функций повставлял, надо в основной чето сделать

--- parsing_Harry.cpp
+++ parsing_Harry.cpp
@@ -1,20 +1,81 @@
-﻿// parsing_Harry.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <algorithm> // эта библиотека нужна для функции transform
+#include <string>
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <set>
+using namespace std;
 
-int main()
+bool IsLetter(string l)
 {
-    std::cout << "Hello World!\n";
+	if ((l >= "A" && l <= "Z") || (l >= "a" && l <= "z")
+		|| (l >= "А" && l <= "Я")
+		|| (l >= "а" && l <= "я")
+		|| (l == "ё")
+		|| (l == "Ё")
+		|| (l == "\'"))
+		return true;
+	return false;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+vector<string> multisplit(const string& str, const set<char>& delimiters = { ' ' })
+{
+	vector<string> result;
+	string part;
+	size_t startPos = 0;
+	size_t pos = multifind(str, delimiters);
+	while (pos != std::string::npos)
+	{
+		part = str.substr(startPos, pos - startPos);
+		if (part.length() > 0)
+			result.push_back(part);
+		startPos = pos + 1;
+		pos = multifind(str, delimiters, pos + 1);
+	}
+	part = str.substr(startPos, str.length() - startPos);
+	if (part.length() > 0)
+		result.push_back(part);
+	return result;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+size_t multifind(const string& str, const set<char>& delimiters, size_t startPos = 0)
+{
+	for (size_t i = startPos; i < str.length(); i++)
+	{
+		if (delimiters.count(str[i]) > 0)
+			return i;
+	}
+	return std::string::npos;
+}
+
+vector<vector<string>> SentencesParser(string text)
+{
+	vector<vector<string>> result;
+
+	
+
+	return result;
+}
+
+void Print(vector<vector<string>> vvs)   //функция вывода координат
+{
+	for (int i = 0; i < vvs.size(); i ++)
+	{
+		
+	}
+}
+
+int main()
+{		
+	setlocale(LC_ALL, "");
+	ifstream f("..\\HarryPotterText.txt");
+	string str, source;
+	if (f)
+	{
+		while (f >> str)
+			auto result1 = multisplit(str, set<char> {'!', '.', '?', ';', ':', '(', ')'});
+	}
+	else return -1;
+
+	transform(source.begin(), source.end(), source.begin(), tolower);
+}
